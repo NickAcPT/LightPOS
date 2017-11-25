ed51ad1f2d0b3f06134b35cda1444e48ada739d9
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2017-11-25 18:37:47 +0000

    Fix namespace

--- LightPOS/Program.cs
+++ LightPOS/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace LightPOS
+namespace NickAc.LightPOS.Frontend
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace LightPOS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
         }
     }
 }
