4133748023de20d8a44cb2d4689409a75aa717e1
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2017-11-26 11:54:04 +0000

    Add icon and start working on GUI

---
+++ LightPOS/Forms/FormMenu.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace NickAc.LightPOS.Frontend.Forms
+{
+    partial class FormMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.appBar1 = new NickAc.ModernUIDoneRight.Controls.AppBar();
+            this.SuspendLayout();
+            // 
+            // appBar1
+            // 
+            this.appBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.appBar1.IconVisible = false;
+            this.appBar1.Location = new System.Drawing.Point(1, 33);
+            this.appBar1.Name = "appBar1";
+            this.appBar1.Size = new System.Drawing.Size(684, 50);
+            this.appBar1.TabIndex = 0;
+            this.appBar1.Text = "appBar1";
+            this.appBar1.TextFont = new System.Drawing.Font("Segoe UI", 14F);
+            // 
+            // FormMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 459);
+            this.ColorScheme.PrimaryColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(189)))), ((int)(((byte)(189)))));
+            this.ColorScheme.SecondaryColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.Controls.Add(this.appBar1);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.MinimumSize = new System.Drawing.Size(96, 39);
+            this.Name = "FormMenu";
+            this.Text = "LightPOS - Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ModernUIDoneRight.Controls.AppBar appBar1;
+    }
+}
---
+++ LightPOS/Forms/FormMenu.cs
@@ -0,0 +1,20 @@
+﻿using NickAc.ModernUIDoneRight.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NickAc.LightPOS.Frontend.Forms
+{
+    public partial class FormMenu : ModernForm
+    {
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
