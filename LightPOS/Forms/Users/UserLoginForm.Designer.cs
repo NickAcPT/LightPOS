1cf16fde8ecc85901fb31bdb0c1c571f16577c1c
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2017-12-01 21:24:18 +0000

    Start to work on user login

---
+++ LightPOS/Forms/Users/UserLoginForm.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace NickAc.LightPOS.Frontend.Forms.Users
+{
+    partial class UserLoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // appBar1
+            // 
+            this.appBar1.Size = new System.Drawing.Size(459, 50);
+            // 
+            // UserLoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 300);
+            this.ColorScheme.PrimaryColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(119)))), ((int)(((byte)(189)))));
+            this.ColorScheme.SecondaryColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(75)))), ((int)(((byte)(120)))));
+            this.Name = "UserLoginForm";
+            this.Text = "UserLoginForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
