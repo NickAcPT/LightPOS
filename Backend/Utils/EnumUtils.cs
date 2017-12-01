8497705f4f2c35345e8af4243b1a5d55530c5d5d
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2017-12-01 20:10:54 +0000

    Add enum utils

--- Backend/Utils/EnumUtils.cs
+++ Backend/Utils/EnumUtils.cs
@@ -2,10 +2,96 @@
 // Copyright (c) NickAc. All rights reserved.
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 //
+using System;
+using System.Reflection;
+
 namespace NickAc.LightPOS.Backend.Utils
 {
-    public class EnumUtils
+    public static class EnumUtils
     {
 
+        #region Methods
+        public static string GetDescription(this Enum value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute[] attributes =
+                (DescriptionAttribute[])fi.GetCustomAttributes(
+                typeof(DescriptionAttribute),
+                false);
+
+            if (attributes != null &&
+                attributes.Length > 0)
+                return attributes[0].Description;
+            return value.ToString();
+        }
+
+        public static Boolean HasDescription(this Enum value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute[] attributes =
+                (DescriptionAttribute[])fi.GetCustomAttributes(
+                typeof(DescriptionAttribute),
+                false);
+
+            return (attributes != null &&
+                attributes.Length > 0);
+        }
+
+        //Code taken from StackOverflow Answer (https://stackoverflow.com/questions/24172370/operator-cannot-be-applied-to-operands-of-type-system-enum-and-system-enu)
+        //All credits go to DLeh(https://stackoverflow.com/users/526704/dleh)
+        public static Enum And(this Enum a, Enum b)
+        {
+            // consider adding argument validation here
+            if (Enum.GetUnderlyingType(a.GetType()) != typeof(ulong))
+                return (Enum)Enum.ToObject(a.GetType(), Convert.ToInt64(a) & Convert.ToInt64(b));
+            else
+                return (Enum)Enum.ToObject(a.GetType(), Convert.ToUInt64(a) & Convert.ToUInt64(b));
+        }
+
+        //Code taken from StackOverflow Answer (https://stackoverflow.com/questions/24172370/operator-cannot-be-applied-to-operands-of-type-system-enum-and-system-enu)
+        //All credits go to DLeh(https://stackoverflow.com/users/526704/dleh)
+        public static Enum Not(this Enum a)
+        {
+            // consider adding argument validation here
+            return (Enum)Enum.ToObject(a.GetType(), ~Convert.ToInt64(a));
+        }
+
+        //Code taken from StackOverflow Answer (https://stackoverflow.com/questions/24172370/operator-cannot-be-applied-to-operands-of-type-system-enum-and-system-enu)
+        //All credits go to DLeh(https://stackoverflow.com/users/526704/dleh)
+        public static Enum Or(this Enum a, Enum b)
+        {
+            // consider adding argument validation here
+            if (Enum.GetUnderlyingType(a.GetType()) != typeof(ulong))
+                return (Enum)Enum.ToObject(a.GetType(), Convert.ToInt64(a) | Convert.ToInt64(b));
+            else
+                return (Enum)Enum.ToObject(a.GetType(), Convert.ToUInt64(a) | Convert.ToUInt64(b));
+        }
+
+        #endregion
+
+        #region Classes
+
+        [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+        public class DescriptionAttribute : System.Attribute
+        {
+            #region Constructors
+
+            public DescriptionAttribute(string description)
+            {
+                Description = description;
+            }
+
+            #endregion
+
+            #region Properties
+
+            public string Description { get; set; }
+
+            #endregion
+        }
+
+        #endregion
     }
 }
