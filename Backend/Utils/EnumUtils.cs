c39c29f1a80d5dc032aa154401d9662a385ae008
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2017-12-01 19:06:04 +0000

    Add EnumUitls class

---
+++ Backend/Utils/EnumUtils.cs
@@ -0,0 +1,11 @@
+﻿//
+// Copyright (c) NickAc. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+//
+namespace NickAc.LightPOS.Backend.Utils
+{
+    public class EnumUtils
+    {
+
+    }
+}
