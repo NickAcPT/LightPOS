3490092062ff71e06a539107d4b1ddf3d7483ac8
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2017-12-03 22:50:27 +0000

    Animations are lit

---
+++ Backend/Utils/Animation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace NickAc.LightPOS.Backend.Utils
+{
+    public class Animation
+    {
+        internal int hitCount;
+        internal int executionLimit;
+        internal Action<Animation> timerAction;
+        internal readonly Timer internalTimer;
+        public Animation()
+        {
+            internalTimer = new Timer(5)
+            {
+                AutoReset = true
+            };
+            internalTimer.Elapsed += (s, e) => {
+                if (executionLimit > 0) {
+                    if (hitCount > executionLimit) {
+                        Stop();
+                        return;
+                    }
+                    hitCount++;
+                }
+                timerAction?.Invoke(this);
+
+            };
+        }
+
+        public Animation WithAction(Action<Animation> action)
+        {
+            timerAction = action;
+            return this;
+        }
+
+        public Animation WithLimit(int limit)
+        {
+            executionLimit = limit;
+            return this;
+        }
+
+        public Animation Start()
+        {
+            internalTimer.Start();
+            return this;
+        }
+
+        public Animation Stop()
+        {
+            internalTimer.Stop();
+            return this;
+        }
+    }
+}
