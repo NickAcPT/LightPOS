ffb25d4fbc5181247ca08f93da442a5d29f61c27
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2018-09-18 21:31:21 +0100

    Some work

---
+++ Backend/Mapping/BaseClassMap.cs
@@ -0,0 +1,18 @@
+﻿// 
+// Copyright (c) NickAc. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+using FluentNHibernate.Mapping;
+using NickAc.LightPOS.Backend.Data;
+
+namespace NickAc.LightPOS.Backend.Mapping
+{
+    public class BaseClassMap<T> : ClassMap<T> where T : BaseDbItem
+    {
+        public BaseClassMap()
+        {
+            Id(x => x.Id).GeneratedBy.Increment();
+            Map(x => x.Deleted).Not.Nullable();
+        }
+    }
+}
